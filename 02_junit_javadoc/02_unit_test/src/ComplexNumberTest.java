dacf6b9243322b1bf9db99197654808d5e78bba1
Oanh Nguyen <oanhnguyen@Oanhs-MacBook-Pro.local>
2016-10-10 15:57:06 +0200

    Test Subtraktion geschrieben, Test Multiplikation geschrieben und Fehler Vorzeichen behoben

--- 02_junit_javadoc/02_unit_test/src/ComplexNumberTest.java
+++ 02_junit_javadoc/02_unit_test/src/ComplexNumberTest.java
@@ -12,17 +12,17 @@ public class ComplexNumberTest {
     @Test
     public void testAddition() {
         // Beispiel für einen Test der Addition
-        
+
         // Durchzuführende Tests:
         // (4 + -5i) + (2 + 11i) = (6 + 6i)
-        // (4 + -5i) + 3         = (7 + -5i)        
+        // (4 + -5i) + 3 = (7 + -5i)
         ComplexNumber a = new ComplexNumber(4, -5);
         ComplexNumber b = new ComplexNumber(2, 11);
 
         assertEquals(new ComplexNumber(6, 6), a.add(b));
         assertEquals(new ComplexNumber(7, -5), a.add(3));
     }
-    
+
     /**
      * Testet die Subtraktion.
      */
@@ -30,9 +30,15 @@ public class ComplexNumberTest {
     public void testSubtraktion() {
         // Durchzuführende Tests:
         // (4 + -5i) - (2 + 11i) = ( 2 + -16i)
-        // (2 + 11i) - (4 + -5i) = (-2 +  16i)
+        // (2 + 11i) - (4 + -5i) = (-2 + 16i)
 
         // TODO: Test schreiben
+        ComplexNumber a = new ComplexNumber(4, -5);
+        ComplexNumber b = new ComplexNumber(2, 11);
+
+        assertEquals(new ComplexNumber(2, -16), a.subtract(b));
+        assertEquals(new ComplexNumber(-2, 16), b.subtract(a));
+
     }
 
     /**
@@ -41,11 +47,16 @@ public class ComplexNumberTest {
     @Test
     public void testMultiplikation() {
         // Durchzuführende Tests:
-        // (2 + -4i) * (-3 + 5i) = (14  +  22i)
-        // (2 +  5i) * ( 3 + 7i) = (-29 +  29i)
-        // (2 + -4i) * 4         = (  8 + -16i)
-        
+        // (2 + -4i) * (-3 + 5i) = (14 + 22i)
+        // (2 + 5i) * ( 3 + 7i) = (-29 + 29i)
+        // (2 + -4i) * 4 = ( 8 + -16i)
+
         // TODO: Test schreiben
+        ComplexNumber a = new ComplexNumber(2, -4);
+        ComplexNumber b = new ComplexNumber(-3, 5);
+        
+        assertEquals(new ComplexNumber(14, 22), a.multiply(b));
+        
     }
 
     /**
@@ -55,9 +66,9 @@ public class ComplexNumberTest {
     public void testDivision() {
         // Durchzuführende Tests:
         // (4 + -8i) : (3 + 4i) = (-0,8 + -1,6i)
-        // (1 +  0i) : (0 + 1i) = (   0 +   -1i)
-        // (8 + 16i) : 4        = (   2 +    4i)
-        
+        // (1 + 0i) : (0 + 1i) = ( 0 + -1i)
+        // (8 + 16i) : 4 = ( 2 + 4i)
+
         // TODO: Test schreiben
     }
 
@@ -67,9 +78,9 @@ public class ComplexNumberTest {
     @Test
     public void testConjugate() {
         // Durchzuführende Tests:
-        // (3   +  5i) --conj--> (3   + -5i)
-        // (-2  + -4i) --conj--> (-2  + 4i)
-          
+        // (3 + 5i) --conj--> (3 + -5i)
+        // (-2 + -4i) --conj--> (-2 + 4i)
+
         // TODO: Test schreiben
     }
 
@@ -80,11 +91,11 @@ public class ComplexNumberTest {
     public void testBetrag() {
         // Durchzuführende Tests:
         // |(3 + -4i)| = 5
-        // |(0 +  3i)| = 3
+        // |(0 + 3i)| = 3
 
         // TODO: Test schreiben
-    }   
-    
+    }
+
     /**
      * Testet das Potenzieren.
      */
@@ -99,4 +110,3 @@ public class ComplexNumberTest {
         // TODO: Test schreiben
     }
 }
-
